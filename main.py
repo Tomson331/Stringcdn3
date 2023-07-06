8d69266fd197a6ff1280b77d3f371e7c1b74d969
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-06 15:22:12 +0200

    Zmienianie wartości zmiennej, argumentów wywołania metody

---
+++ main.py
@@ -0,0 +1,20 @@
+# Day 10 - String cdn3
+
+nazwaBloga = "Zero To Junior"
+
+print(nazwaBloga.upper())
+print(nazwaBloga.lower())
+print(nazwaBloga.startswith("Junior"))
+print(nazwaBloga.endswith("Junior"))
+print(nazwaBloga.count("o"))
+
+# Zadanie
+print(nazwaBloga.lower())
+print(nazwaBloga.upper())
+
+print(nazwaBloga.count("z"))
+print(nazwaBloga.startswith("Zero"))
+print(nazwaBloga.endswith("Zero"))
+print(nazwaBloga.count("e"))
+
+
